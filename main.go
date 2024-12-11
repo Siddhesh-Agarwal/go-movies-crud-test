79eeca366b7540a5d5658b5eea6b38137c169105
Siddhesh Agarwal <siddhesh.agarwal@gmail.com>
2024-12-11 14:54:56 +0530

    added ORM support

--- main.go
+++ main.go
@@ -3,22 +3,22 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Movie struct {
+	gorm.Model
 	ID    int
 	Title string
 	Year  int
 	Genre string
 }
-
-var movies []Movie
 
 func to_int(s string) int {
 	i, err := strconv.Atoi(s)
@@ -28,76 +28,94 @@ func to_int(s string) int {
 	return i
 }
 
+func getDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("./temp.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
+
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var movies []Movie
+	db := getDB()
+	db.Find(&movies)
 	json.NewEncoder(w).Encode(movies)
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
 	params := mux.Vars(r)
-	for index, item := range movies {
-		if item.ID == to_int(params["id"]) {
-			movies = append(movies[:index], movies[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(movies)
+	movie_id := to_int(params["id"])
+	db := getDB()
+	db.Delete(&movie, movie_id)
+	json.NewEncoder(w).Encode(movie)
 }
 
 func getMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	var movie Movie
 	params := mux.Vars(r)
-	for _, item := range movies {
-		if item.ID == to_int(params["id"]) {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
+	movie_id := to_int(params["id"])
+	db := getDB()
+	db.First(&movie, movie_id)
+	json.NewEncoder(w).Encode(movie)
 }
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
-	// Movie ID is a random number
-	movie.ID = rand.Intn(10000000)
-	// println(movie.ID)
-	movies = append(movies, movie)
+	db := getDB()
+	db.Create(&movie)
 	json.NewEncoder(w).Encode(movie)
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range movies {
-		movie_id := to_int(params["id"])
-		if item.ID == movie_id {
-			movies = append(movies[:index], movies[index+1:]...)
-			var movie Movie
-			_ = json.NewDecoder(r.Body).Decode(&movie)
-			movie.ID = movie_id
-			movies = append(movies, movie)
-			json.NewEncoder(w).Encode(movie)
-			return
-		}
+	var movie Movie
+	movie_id := to_int(params["id"])
+	db := getDB()
+	db.First(&movie, movie_id)
+	if db.RowsAffected == 0 {
+		http.Error(w, "Movie not found", http.StatusNotFound)
+		return
 	}
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	db.Save(&movie)
+	json.NewEncoder(w).Encode(movie)
 }
 
 func main() {
+	// Connect to the database
+	db, err := gorm.Open(sqlite.Open("./temp.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	// Auto migrate the database to match the struct definitions
+	db.AutoMigrate(&Movie{})
+	var movies []Movie
+	// Add some movies to the database if they don't exist
+	if db.Find(&movies).RowsAffected == 0 {
+		db.Create(&Movie{ID: 1, Title: "The Shawshank Redemption", Year: 1994, Genre: "Drama"})
+		db.Create(&Movie{ID: 2, Title: "The Godfather", Year: 1972, Genre: "Crime"})
+		db.Create(&Movie{ID: 3, Title: "The Dark Knight", Year: 2008, Genre: "Action"})
+	}
+
 	port, port_exists := os.LookupEnv("PORT")
 	if !port_exists {
 		port = "8000"
 	}
-
 	r := mux.NewRouter()
-	movies = append(movies, Movie{ID: 1, Title: "The Shawshank Redemption", Year: 1994, Genre: "Drama"})
-	movies = append(movies, Movie{ID: 2, Title: "The Godfather", Year: 1972, Genre: "Crime"})
-	movies = append(movies, Movie{ID: 3, Title: "The Dark Knight", Year: 2008, Genre: "Action"})
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
 	fmt.Printf("Starting server at port %s\n", port)
+	fmt.Printf("Local: http://localhost:%s\n", port)
+	http.ListenAndServe(":"+port, r)
 }
